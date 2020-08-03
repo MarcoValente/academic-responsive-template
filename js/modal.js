81fa85fa5dceeada9243d5559dbf82282aef484f
Marco Valente <marco.valente@cern.ch>
2020-08-03 21:22:13 +0200

    Adding modal for all images

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,11 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+
+var modal = document.getElementById("myModal"+number);
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("imgmodal");
-var captionText = document.getElementById("caption");
+var img = document.getElementById("myImg" + number);
+var modalImg = document.getElementById("imgmodal" + number);
+var captionText = document.getElementById("caption"+number);
 img.onclick = function () {
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -12,7 +13,7 @@ img.onclick = function () {
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementById("close"+number);
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
